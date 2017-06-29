867fe4da8d12b6b0380a0af0e70b8dd1ff52a6d3
EMayorga <emayorgamolina@gmail.com>
2017-06-29 17:50:01 +0200

    archivos
    renombrado de archivos

---
+++ 2 -GraficasDatos.R
@@ -0,0 +1,205 @@
+
+## Archivo donde se pintaran graficas de las variables del dataset con el fin de analizarlas.
+
+setwd("C:/Users/epifanio.mayorga/Desktop/Master/TFM") ## ruta curro
+
+
+## Apertura del dataset
+vuelos <- read.table("vuelosDeparted.csv", header = T, sep = ",")
+
+str(vuelos)
+
+## Eliminamos variables creadas al guardar el fichero
+vuelos$X <- NULL
+vuelos$index <- NULL
+
+str(vuelos)
+summary(vuelos)
+
+
+
+
+
+
+
+
+## Graficas de variables
+library(ggplot2)
+
+
+# Variables categoricas
+## Funcion que calcula el retraso medio de los valores indicados en la variable de entrada numVuelos e informa
+## de la cantidad de vuelos que ha realizado un avion.
+## Devuelve un dataframe que almacena el valor de entrada, su retraso medio y el numero de apariciones (o vuelos)
+mediasRetrasos <- function(numVuelos, muestra){
+  vectorCodigos <- vector()
+  vectorRetrasos <- vector()
+  vectorNumeroVuelos <- vector()
+  
+  ## bucle for por cada codigo de vuelo
+  for(i in 1:length(numVuelos)){
+    
+    vuelos <- muestra[muestra[,2]==numVuelos[i],]
+    retrasoMedio <- 0
+    
+    if(length(vuelos[,1])>0){
+      retrasoMedio <- mean(vuelos[,1])
+    }
+    
+    vectorCodigos[i] = as.character(numVuelos[i])
+    vectorRetrasos[i] = as.numeric(retrasoMedio)    
+    vectorNumeroVuelos[i] <- length(vuelos[,1])
+    
+  }
+  df <- as.data.frame(list(vectorCodigos,vectorRetrasos,vectorNumeroVuelos), col.names = c("codigo","retrasoMedio","numeroVuelos"))
+  return(df)
+}
+
+# Flight_number
+df <- subset(vuelos, select = c("arrival_delay","flight_number"))
+variables <- unique(df$flight_number)
+mediaFlightNumber <- mediasRetrasos(variables,df)
+
+# BoardPoint
+df <- subset(vuelos, select = c("arrival_delay","board_point"))
+variables <- unique(df$flight_number)
+mediaBoardPoint <- mediasRetrasos(variables,df)
+
+# BoardLat
+df <- subset(vuelos, select = c("arrival_delay","board_lat"))
+variables <- unique(df$flight_number)
+mediaBoardLat <- mediasRetrasos(variables,df)
+
+# BoardLon
+df <- subset(vuelos, select = c("arrival_delay","board_lon"))
+variables <- unique(df$flight_number)
+mediaBoardLon <- mediasRetrasos(variables,df)
+
+# BoardCountryCode  
+df <- subset(vuelos, select = c("arrival_delay","board_country_code"))
+variables <- unique(df$flight_number)
+mediaBoardCC <- mediasRetrasos(variables,df)
+  
+# OffPoint 
+df <- subset(vuelos, select = c("arrival_delay","off_point"))
+variables <- unique(df$flight_number)
+mediaOffPoint <- mediasRetrasos(variables,df)
+
+# OffLat
+df <- subset(vuelos, select = c("arrival_delay","off_lat"))
+variables <- unique(df$flight_number)
+mediaOffLat <- mediasRetrasos(variables,df)
+
+# OffLon
+df <- subset(vuelos, select = c("arrival_delay","off_lon"))
+variables <- unique(df$flight_number)
+mediaOffLon <- mediasRetrasos(variables,df)
+  
+# OffCountryCode
+df <- subset(vuelos, select = c("arrival_delay","off_country_code"))
+variables <- unique(df$flight_number)
+mediaOffCC <- mediasRetrasos(variables,df)
+  
+# AircraftType
+df <- subset(vuelos, select = c("arrival_delay","aircraft_type"))
+variables <- unique(df$flight_number)
+mediaAircraftType <- mediasRetrasos(variables,df)
+  
+# AircraftRegistrationNumber
+df <- subset(vuelos, select = c("arrival_delay","aircraft_registration_number"))
+variables <- unique(df$flight_number)
+mediaAircraftRegistratioNumber <- mediasRetrasos(variables,df)
+  
+# Routing
+df <- subset(vuelos, select = c("arrival_delay","routing"))
+variables <- unique(df$flight_number)
+mediaRouting <- mediasRetrasos(variables,df)
+
+# MesSalida
+df <- subset(vuelos, select = c("arrival_delay","mesSalida"))
+variables <- unique(df$flight_number)
+mediaMesSalida <- mediasRetrasos(variables,df)
+  
+# DiaSalida
+df <- subset(vuelos, select = c("arrival_delay","diaSalida"))
+variables <- unique(df$flight_number)
+mediaDiaSalida <- mediasRetrasos(variables,df)
+
+# HoraSalida 
+df <- subset(vuelos, select = c("arrival_delay","horaSalida"))
+variables <- unique(df$flight_number)
+mediaHoraSalida <- mediasRetrasos(variables,df)
+  
+# MesLlegada
+df <- subset(vuelos, select = c("arrival_delay","mesLlegada"))
+variables <- unique(df$flight_number)
+mediaMesLlegada <- mediasRetrasos(variables,df)
+  
+# DiaLlegada
+df <- subset(vuelos, select = c("arrival_delay","diaLlegada"))
+variables <- unique(df$flight_number)
+mediaDiaLlegada <- mediasRetrasos(variables,df)
+
+# HoraLlegada
+df <- subset(vuelos, select = c("arrival_delay","horaLlegada"))
+variables <- unique(df$flight_number)
+mediaHoraLlegada <- mediasRetrasos(variables,df)
+  
+# DiaSemanaSalida
+df <- subset(vuelos, select = c("arrival_delay","diaSemanaSalida"))
+variables <- unique(df$flight_number)  
+mediaDiaSemanaSalida <- mediasRetrasos(variables,df)
+
+# DiaSemanaLlegada
+df <- subset(vuelos, select = c("arrival_delay","diaSemanaLlegada"))
+variables <- unique(df$flight_number)
+mediaDiaSemanaLlegada <- mediasRetrasos(variables,df)
+
+
+## graficas de retraso de llegada en base a variables categoricas
+ggplot(vuelos,aes(airline_code,arrival_delay)) + geom_violin(scale = "count")
+#ggplot(vuelos,aes(flight_number,arrival_delay)) + geom_violin(scale = "count")
+#ggplot(vuelos,aes(board_point,arrival_delay)) + geom_violin(scale = "count")
+#ggplot(vuelos,aes(board_country_code,arrival_delay)) + geom_violin(scale = "count")
+#ggplot(vuelos,aes(off_point,arrival_delay)) + geom_violin(scale = "count")
+#ggplot(vuelos,aes(off_country_code,arrival_delay)) + geom_violin(scale = "count")
+ggplot(vuelos,aes(aircraft_type,arrival_delay)) + geom_violin(scale = "count")
+#ggplot(vuelos,aes(aircraft_registration_number,arrival_delay)) + geom_violin(scale = "count")
+#### ggplot(vuelos,aes(general_status_code,arrival_delay)) + geom_violin(scale = "count") no tiene sentido
+#ggplot(vuelos,aes(routing,arrival_delay)) + geom_violin(scale = "count")
+#### ggplot(vuelos,aes(cabin_1_code,arrival_delay)) + geom_violin(scale = "count") no tiene sentido
+ggplot(vuelos,aes(cabin_2_code,arrival_delay)) + geom_violin(scale = "count")
+ggplot(vuelos,aes(mesSalida,arrival_delay)) + geom_violin(scale = "count")
+ggplot(vuelos,aes(anyoSalida,arrival_delay)) + geom_violin(scale = "count")
+ggplot(vuelos,aes(diaSalida,arrival_delay)) + geom_violin(scale = "count")
+#ggplot(vuelos,aes(horaSalida,arrival_delay)) + geom_violin(scale = "count")
+ggplot(vuelos,aes(mesLlegada,arrival_delay)) + geom_violin(scale = "count")
+ggplot(vuelos,aes(anyoLlegada,arrival_delay)) + geom_violin(scale = "count")
+ggplot(vuelos,aes(diaLlegada,arrival_delay)) + geom_violin(scale = "count")
+#ggplot(vuelos,aes(horaLlegada,arrival_delay)) + geom_violin(scale = "count")
+ggplot(vuelos,aes(diaSemanaSalida,arrival_delay)) + geom_violin(scale = "count")
+ggplot(vuelos,aes(diaSemanaLlegada,arrival_delay)) + geom_violin(scale = "count")
+
+
+# Variables numericas
+## graficas de retraso en base a variables numericas
+ggplot(vuelos, aes(x = board_lat, y = arrival_delay)) + geom_point() + geom_smooth(method = "lm", se=TRUE, color="red", formula = y ~ x)
+ggplot(vuelos, aes(x = board_lon, y = arrival_delay)) + geom_point() + geom_smooth(method = "lm", se=TRUE, color="red", formula = y ~ x)
+ggplot(vuelos, aes(x = off_lat, y = arrival_delay)) + geom_point() + geom_smooth(method = "lm", se=TRUE, color="red", formula = y ~ x)
+ggplot(vuelos, aes(x = off_lon, y = arrival_delay)) + geom_point() + geom_smooth(method = "lm", se=TRUE, color="red", formula = y ~ x)
+ggplot(vuelos, aes(x = distance, y = arrival_delay)) + geom_point() + geom_smooth(method = "lm", se=TRUE, color="red", formula = y ~ x)
+ggplot(vuelos, aes(x = departure_delay, y = arrival_delay)) + geom_point() + geom_smooth(method = "lm", se=TRUE, color="red", formula = y ~ x)
+ggplot(vuelos, aes(x = act_blocktime, y = arrival_delay)) + geom_point() + geom_smooth(method = "lm", se=TRUE, color="red", formula = y ~ x)
+ggplot(vuelos, aes(x = cabin_1_fitted_configuration, y = arrival_delay)) + geom_point() + geom_smooth(method = "lm", se=TRUE, color="red", formula = y ~ x)
+ggplot(vuelos, aes(x = cabin_1_saleable, y = arrival_delay)) + geom_point() + geom_smooth(method = "lm", se=TRUE, color="red", formula = y ~ x)
+ggplot(vuelos, aes(x = cabin_1_pax_boarded, y = arrival_delay)) + geom_point() + geom_smooth(method = "lm", se=TRUE, color="red", formula = y ~ x)
+ggplot(vuelos, aes(x = cabin_1_rpk, y = arrival_delay)) + geom_point() + geom_smooth(method = "lm", se=TRUE, color="red", formula = y ~ x)
+ggplot(vuelos, aes(x = cabin_1_ask, y = arrival_delay)) + geom_point() + geom_smooth(method = "lm", se=TRUE, color="red", formula = y ~ x)
+ggplot(vuelos, aes(x = load_factor, y = arrival_delay)) + geom_point() + geom_smooth(method = "lm", se=TRUE, color="red", formula = y ~ x)
+ggplot(vuelos, aes(x = total_pax, y = arrival_delay)) + geom_point() + geom_smooth(method = "lm", se=TRUE, color="red", formula = y ~ x)
+ggplot(vuelos, aes(x = total_no_shows, y = arrival_delay)) + geom_point() + geom_smooth(method = "lm", se=TRUE, color="red", formula = y ~ x)
+ggplot(vuelos, aes(x = total_cabin_crew, y = arrival_delay)) + geom_point() + geom_smooth(method = "lm", se=TRUE, color="red", formula = y ~ x)
+ggplot(vuelos, aes(x = total_technical_crew, y = arrival_delay)) + geom_point() + geom_smooth(method = "lm", se=TRUE, color="red", formula = y ~ x)
+ggplot(vuelos, aes(x = total_baggage_weight, y = arrival_delay)) + geom_point() + geom_smooth(method = "lm", se=TRUE, color="red", formula = y ~ x)
+ggplot(vuelos, aes(x = number_of_baggage_pieces, y = arrival_delay)) + geom_point() + geom_smooth(method = "lm", se=TRUE, color="red", formula = y ~ x)
+
